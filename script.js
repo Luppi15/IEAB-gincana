a8fcebb8f513fbf867fee7fc55a6e31d83809427
Luppi15 <luppister15@gmail.com>
2025-02-19 14:05:40 -0400

    🌎 Mudança nas perguntas

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const items = [
     { question: "Eu tive 700 esposas e 300 concubinas", answer: "Salomão" },
     { question: "Fui engolido por um grande peixe e sobrevivi", answer: "Jonas" },
-    { question: "Fui vendido pelos meus irmãos e me tornei governador no Egito", answer: "José" },
+    { question: "Saí no soco com um anjo e ganhei um nome novo.", answer: "Jacó" },
     { question: "Derrubei um gigante com apenas uma pedra", answer: "Davi" },
     { question: "Construí uma grande arca antes do dilúvio", answer: "Noé" },
     { question: "Traí Jesus por 30 moedas de prata", answer: "Judas Iscariotes" },
     { question: "Fui o primeiro homem criado por Deus", answer: "Adão" },
-    { question: "Recebi os Dez Mandamentos no Monte Sinai", answer: "Moisés" },
-    { question: "Neguei Jesus três vezes antes do galo cantar", answer: "Pedro" },
-    { question: "Meu nome era Saulo antes de minha conversão", answer: "Paulo" }
+    { question: "Fiz um voto de nunca cortar o cabelo, mas minha namorada me passou a tesoura.", answer: "Sansão" },
+    { question: "Minha esposa riu quando Deus disse que teríamos um filho.", answer: "Abraão" }
 ];
 
 let currentAnswer = "";
